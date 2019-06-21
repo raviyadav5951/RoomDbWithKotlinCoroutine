e11f58dfdeebe845e15272c74dbfbca443890c5e
Ravi Yadav <raviyadav5951@gmail.com>
2019-06-21 14:49:47 +0530

    fetch the count using coroutine

--- app/src/main/java/com/test/roomdbkotlin/activity/MainActivity.kt
+++ app/src/main/java/com/test/roomdbkotlin/activity/MainActivity.kt
@@ -3,15 +3,15 @@ package com.test.roomdbkotlin.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.test.roomdbkotlin.R
 import com.test.roomdbkotlin.database.AppDatabase
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = MainActivity::class.java.simpleName
     private var dbInstance: AppDatabase? = null
     private var totalRecords: Int = 0
 
@@ -42,15 +42,51 @@ class MainActivity : AppCompatActivity() {
         //separate thread to perform operation on database
         GlobalScope.launch(Dispatchers.IO) {
             val data = dbInstance?.studentDao()?.getAllStudentRecords()
-            totalRecords=data?.size as Int
+            totalRecords = data?.size as Int
+            Log.e(TAG, "size==$totalRecords")
         }
 
     }
 
+    /**
+     * Function to calculate the total records in the students table
+     * Using Async and await
+     */
+    private suspend fun fetchRecordsUsingAsync(): Int {
+        val result = GlobalScope.async(Dispatchers.IO) {
+            val data = dbInstance?.studentDao()?.getAllStudentRecords()
+            totalRecords = data?.size as Int
+//            Log.e(TAG,"size==$totalRecords")
+        }
+        result.await()
+        return totalRecords
+    }
+
+    private suspend fun fetchRecordsUsingWithContext(): Int {
+        val result = withContext(Dispatchers.IO)
+        {
+            val data = dbInstance?.studentDao()?.getAllStudentRecords()
+            totalRecords = data?.size as Int
+        }
+        return totalRecords
+    }
+
     override fun onResume() {
         super.onResume()
-        fetchTotalRecords()
-        tvTotalRecords.text = "Total records in database=$totalRecords"
+
+        GlobalScope.launch(Dispatchers.Main) {
+            //using async
+            val students: Int = fetchRecordsUsingAsync() // fetch on IO thread
+
+            //using withContext
+            //val students: Int = fetchRecordsUsingWithContext() // fetch on IO thread
+            displayRecordsCount(students) // back on UI thread
+        }
+    }
+
+    private fun displayRecordsCount(students: Int) {
+
+        tvTotalRecords.text = "Total records in database=$students"
     }
 }
 
--- app/src/main/java/com/test/roomdbkotlin/adapter/ViewStudentsAdapter.kt
+++ app/src/main/java/com/test/roomdbkotlin/adapter/ViewStudentsAdapter.kt
@@ -1,13 +1,4 @@
 package com.test.roomdbkotlin.adapter
 
-import android.support.v7.widget.RecyclerView
-import android.view.ViewGroup
-
-class ViewStudentsAdapter:RecyclerView.Adapter<> {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): [
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onBindViewHolder(p0: [ERROR: No type element]
+class ViewStudentsAdapter{
 }
